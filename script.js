a8c27aaddd5366b89242defe326dc266dd581440
Derek Shoemaker <shoemakerdr@gmail.com>
2017-05-25 22:58:36 -0500

    added outline for render functions

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 (function () {
 	// DOMContentLoaded event listener
 	document.addEventListener("DOMContentLoaded", () => {
+
 		// define elements
 		const channels = ["esl_sc2", "ogamingsc2", "cretetion", "freecodecamp",
 		                  "storbeck", "habathcx", "robotcaleb", "noobs2ninjas",
@@ -14,8 +15,22 @@
 			.catch(err => console.log(err))
 		)).then(res => console.log(res));
 
-		// render function
+		// rendering functions
+
+        // create channel section for each channel
+        function createChannelSection(channelData) {
+            // define elements that will be made in this render function
+            // pull properties out of obj and use them to render
+
+        }
+
+        // append channel sections together
+
+        // append channel sections to "ul" node in DOM
+
+
 
 		// bind events function(s)
+
 	});
 })();
